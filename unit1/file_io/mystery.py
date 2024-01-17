a5286d1f5fe95e1b623817c6340001217e2ec5f3
Yoshi <rytaneja@gmail.com>
2024-01-16 23:36:23 -0800

    1/16 file io

--- unit1/file_io/mystery.py
+++ unit1/file_io/mystery.py
@@ -1,8 +1,18 @@
-myFile = open('mystery.txt')
+myFile = open('unit1/file_io/mystery.txt')
 # print(myFile)
 stringFile = myFile.read()
 words = stringFile.split()
 sentences = stringFile.splitlines()
 
-with open("mystery.txt") as f:
+with open("unit1/file_io/mystery.txt") as f:
     lines = f.read().splitlines()
+
+for i in range(10):
+    print(lines[i])
+
+
+count = 0
+for line in lines:
+    if "the" in line:
+        count += 1
+print(count)
