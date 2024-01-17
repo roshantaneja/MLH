2ff3ee79df3664e10ef6cc7744da54dfabd9b7c0
Yoshi <rytaneja@gmail.com>
2024-01-16 23:26:55 -0800

    1/16 Search Algos

---
+++ unit1/bacon/lab.py
@@ -0,0 +1,28 @@
+import json
+
+def make_actor_dictionary(data):
+    raise NotImplementedError("Implement me!")
+
+def did_x_and_y_act_together(data, actor_id_1, actor_id_2):
+    raise NotImplementedError("Implement me!")
+
+def get_actors_with_bacon_number(data, n):
+    raise NotImplementedError("Implement me!")
+
+def get_bacon_path(data, actor_id):
+    raise NotImplementedError("Implement me!")
+
+def get_path(data, actor_id_1, actor_id_2):
+    raise NotImplementedError("Implement me!")
+
+def actor_path(data, path):
+    raise NotImplementedError("Implement me!")
+
+def get_movie_path(data, actor_id_1, actor_id_2):
+    raise NotImplementedError("Implement me!")
+
+if __name__ == '__main__':
+    # additional code here will be run only when lab.py is invoked directly
+    # (not when imported from test.py), so this is a good place to put code
+    # used, for example, to generate the results for the online questions.
+    pass
